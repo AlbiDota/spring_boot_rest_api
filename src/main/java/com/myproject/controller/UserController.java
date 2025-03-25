63818c4a68abae48cc528e776c86c2310365b2bf
AlbiDota <alisaele1@gmail.com>
2025-03-25 22:51:56 +0100

    Progress on UserController

--- src/main/java/com/myproject/controller/UserController.java
+++ src/main/java/com/myproject/controller/UserController.java
@@ -1,4 +1,30 @@
 package com.myproject.controller;
 
+import com.myproject.model.User;
+import com.myproject.repository.UserRepository;
+import com.myproject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+@Tag(name = "User Controller", description = "API for users")
 public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+
 }
