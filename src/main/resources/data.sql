a5242c6c8b55635ff957eaaca4cc70a8a2ca2c89
AlbiDota <alisaele1@gmail.com>
2025-03-26 15:41:02 +0100

    working on database stuff

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,6 @@
 DELETE FROM User;
 
 -- Sample data for Product entity
-INSERT INTO product (email, password, firstname, lastname) VALUES
+INSERT INTO product ( email, password, firstname, lastname) VALUES
     ('albert@oblig2.no', 'PepsiMax123!', 'Albert', 'Dota'),
     ('ikkealbert@oblig2.no', 'CocaCola123!', 'Ikke', 'Albert');
